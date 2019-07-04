c6f435fbc8f5f09bb22c1139f00295862ac109c4
kbz <18211571509@163.com>
2019-07-04 18:18:52 +0800

    增加图片

--- lib/welcome_page/welcome_page.dart
+++ lib/welcome_page/welcome_page.dart
@@ -2,8 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -33,64 +31,56 @@ class WelcomePage extends StatelessWidget {
           child: Image.asset("assets/images/img_buy_onestep.png"),
         ),
         Align(
-          alignment: Alignment(0.25, -0.12),
-          child: Image.asset("assets/images/img_meet_onestep.png")
-        ),
-
-        Align(
-            alignment: Alignment(-0.3, 0.13),
-            child: ImageMoveWidget()
-        ),
+            alignment: Alignment(0.25, -0.12),
+            child: Image.asset("assets/images/img_meet_onestep.png")),
+        Align(alignment: Alignment(-0.3, 0.13), child: ImageMoveWidget()),
       ],
     );
   }
-
 }
-class ImageMoveWidget extends StatefulWidget{
 
-
+class ImageMoveWidget extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return ImageMoveWidgetState();
   }
-
 }
-class ImageMoveWidgetState extends State<ImageMoveWidget> with SingleTickerProviderStateMixin{
 
-
-
+class ImageMoveWidgetState extends State<ImageMoveWidget>
+    with SingleTickerProviderStateMixin {
   AnimationController _controller;
-  Animation _animation;
+  Animation<EdgeInsets> _animation;
 
   @override
   Widget build(BuildContext context) {
-
-
-
     return AnimatedBuilder(
-
-      builder: (BuildContext context, Widget child){
-        print("_animation       "+_animation.value.toString());
+      builder: (BuildContext context, Widget child) {
+        print("_animation       " + _animation.value.left.toString());
         return Container(
-          padding: EdgeInsets.only(left: _animation.value),
-          child: Image.asset("assets/images/img_eat_onestep.png"),
 
+          transform: new Matrix4.translationValues(_animation.value.left, 0, 0),
+          child: GestureDetector(
+            child: Image.asset("assets/images/img_eat_onestep.png"),
+            onTap: (){
+
+            },
+          ),
         );
       },
       animation: _animation,
-
     );
   }
 
   @override
   void initState() {
     super.initState();
-    _controller=AnimationController(vsync: this,duration:Duration(milliseconds: 5000) );
-    _animation=Tween(begin: 300.0,end: 0.0).animate(_controller);
+    _controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1000));
+    _animation = EdgeInsetsTween(
+      begin: EdgeInsets.only(left: 350.0),
+      end: EdgeInsets.only(left: 0.0),
+    ).animate(_controller);
     _controller.forward();
   }
-
 }
-
-
